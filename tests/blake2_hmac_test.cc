a1a23ee5075a58755a7caaf5e4560cd50bd78fc9
Tomasz Babiuk <tomasz.babiuk@gmail.com>
2022-11-26 21:22:40 +0100

    adding gmock to achieve a fiesr test using ElementsAre matcher

--- tests/blake2_hmac_test.cc
+++ tests/blake2_hmac_test.cc
@@ -1,7 +1,10 @@
 #include "hmac_blake2s.h"
+#include "gmock/gmock.h"
 #include <gtest/gtest.h>
 #include <stdio.h>
 #include <string.h>
+
+using ::testing::ElementsAre;
 
 uint8_t secret[32] = {2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2,
                       2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2};
@@ -14,13 +17,19 @@ uint8_t salt_96[96] = {
 uint8_t salt_32[32] = {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
                        1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1};
 
-TEST(HMAC_blake_test, SaltLowerThan32) {
-  uint8_t prs[128] = {0};
-  BLAKE2s_hmac(&secret, 32, &salt_32, 32, (uint8_t *)&prs);
+uint8_t expected_prk_32[32] = {
+    132, 8,  2,   160, 85, 73,  181, 155, 61, 238, 5,  166, 89,  26, 120, 28,
+    30,  91, 213, 124, 57, 118, 241, 44,  96, 108, 95, 64,  238, 61, 83,  137};
+
+TEST(HMAC_blake_test, SaltLowerOrEqual32Characters) {
+  uint8_t prk[32] = {0};
+  BLAKE2s_hmac(&secret, 32, &salt_32, 32, (uint8_t *)&prk);
+
+  ASSERT_THAT(prk, ElementsAre(132, 8, 2, 160, 85, 73, 181, 155, 61, 238, 5,
+                               166, 89, 26, 120, 28, 30, 91, 213, 124, 57, 118,
+                               241, 44, 96, 108, 95, 64, 238, 61, 83, 137));
 
   uint8_t x;
-  EXPECT_STRNE("hello", "world");
-  EXPECT_EQ(7 * 6, 42);
 }
 
 int main(int argc, char *argv[]) {
